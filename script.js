372de90bdd25f2acd659f378d13fb656238e67f0
wafaa909 <wafaa_gu90@hotmail.com>
2022-12-07 19:33:10 +0300

    done

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("مرحباً بك في موقعي! انا (وفاء)، وسوف احسب درجتك")
+
+let grade = prompt();
+   
+ 
+  //جافاسكربت ليست جافا
+
+  
+     if (grade >= 90){
+            console.log('your grade is A')
+     }else if (grade >= 80){
+            console.log('your grade is B')
+     }else if (grade >= 70){
+        console.log('your grade is C')
+ }else if (grade >= 60){
+    console.log('your grade is D')
+}else if (grade >= 50){
+    console.log('your grade is F')
+}else {
+    console.log('Fail')
+}
